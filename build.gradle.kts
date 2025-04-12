9565284886912f4c1f7dd3d11df6b527bacb3321
David Savič <davidsavic8@gmail.com>
2025-04-12 13:10:19 +0200

    Better kafka running instance management with mulitple gradle tasks

--- build.gradle.kts
+++ build.gradle.kts
@@ -16,17 +16,6 @@ subprojects {
     }
 }
 
-tasks.register("startKafka") {
-    group = "docker"
-    description = "Run only Kafka broker by docker-compose.kafka.yml in WSL"
-
-    doLast {
-        exec {
-            commandLine("wsl", "docker-compose", "-f", "docker-compose.kafka.yml", "up")
-        }
-    }
-}
-
 tasks.register("startAll") {
     group = "docker"
     description = "Run whole stack (Kafka + all microservices) in WSL"
@@ -34,6 +23,50 @@ tasks.register("startAll") {
     doLast {
         exec {
             commandLine("wsl", "docker-compose", "up", "--build")
+        }
+    }
+}
+
+tasks.register("startKafka") {
+    group = "docker"
+    description = "Run only Kafka broker by docker-compose.kafka.yml in WSL in detached mode"
+
+    doLast {
+        exec {
+            commandLine("wsl", "docker-compose", "-f", "docker-compose.kafka.yml", "up", "-d", "--remove-orphans")
+        }
+    }
+}
+
+tasks.register("stopKafka") {
+    group = "docker"
+    description = "Stop and remove Kafka container"
+
+    doLast {
+        exec {
+            commandLine("wsl", "docker-compose", "-f", "docker-compose.kafka.yml", "down")
+        }
+    }
+}
+
+tasks.register("viewKafkaLogs") {
+    group = "docker"
+    description = "Tail logs from Kafka container"
+
+    doLast {
+        exec {
+            commandLine("wsl", "docker", "logs", "-f", "kafka")
+        }
+    }
+}
+
+tasks.register("listKafkaTopics") {
+    group = "docker"
+    description = "List all Kafka topics from the running Kafka container"
+
+    doLast {
+        exec {
+            commandLine("wsl", "docker", "exec", "kafka", "kafka-topics.sh", "--bootstrap-server", "localhost:9092", "--list")
         }
     }
 }
